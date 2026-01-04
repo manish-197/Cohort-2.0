3c28d4d6a17bae404c112a5b01866261a784fad9
MANISH PATIL <manishpatil0738@outlook.com>
2026-01-04 21:55:53 +0530

    day 079 complete

--- REACT.JS/DAY 079 - Routing in React/src/components/Navbar.jsx
+++ REACT.JS/DAY 079 - Routing in React/src/components/Navbar.jsx
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='flex '>
-        <h2>
+    <nav className='flex items-center justify-between px-5 bg-sky-400 font-semibold text-{18px} text-black py-2 align-items-center'>
+        <h2 className='text-lg '>
             Navbar
         </h2>
-        <ul>
-            <Link>Home</Link>
-            <Link>Home</Link>
-            <Link>Home</Link>
+        <ul className='flex gap-4 font-semibold text-{18px}'>
+            <Link className='cursor-pointer hover:scale-90 p-1 px-3 border-1 transition-all border-white rounded-3xl bg-blue-500'
+            to='/' >Home</Link>
+            <Link className='cursor-pointer hover:scale-90 p-1 px-3 border-1 transition-all border-white rounded-3xl bg-blue-500'
+            to='/about'>About</Link>
+            <Link className='cursor-pointer hover:scale-90 p-1 px-3 border-1 transition-all border-white rounded-3xl bg-blue-500'
+            to='/contact'>Contact</Link>
         </ul>
     </nav>
   )
---
+++ REACT.JS/DAY 079 - Routing in React/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+         <h1 className='text-center font-semibold text-2xl'>Contact page</h1>
+    </div>
+  )
+}
+
+export default About
