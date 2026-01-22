b2a715041d78de5b9d19b816c92c3819fde3b0f0
MANISH PATIL <manishpatil0738@outlook.com>
2026-01-22 20:00:26 +0530

    day 86 complete

---
+++ BACKEND/DAY 086 - Programming Server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+app.get('/about', (req, res) => {
+    res.send('This is about page')
+})
+app.get('/contact', (req, res) => {
+    res.send('This is contact page')
+})
+
+app.listen(3000)
