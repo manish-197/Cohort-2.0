01f61513d625f6e9781db8bfb12e9003e87957b0
MANISH PATIL <manishpatil0738@outlook.com>
2026-02-23 13:04:26 +0530

    DAY 098 - COMPLETE

---
+++ BACKEND/DAY 098 - Bycrypt/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const connectDB = async () =>{
+   await mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('connected to DB');
+    })
+}
+
+module.exports = connectDB;
---
+++ BACKEND/DAY 098 - Bycrypt/routes/auth.routes.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const userModel = require("../model/user.model");
+const jwt = require("jsonwebtoken");
+const authRouter = express.Router();
+const crypto = require("crypto")
+
+
+authRouter.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const isUserExist = await userModel.findOne({ email })
+
+    if(isUserExist){
+        return res.status(400).json({
+            message: "User already exists"
+        })
+    }
+
+    const hashPassword = crypto.createHash("md5").update(password).digest("hex");
+    
+    
+    const user = await userModel.create({
+        name, email, password: hashPassword
+    });
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET_KEY,
+    )
+    
+    res.cookie("jwt_token", token)
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user,
+        token
+    });
+});
+
+authRouter.post("/login", async (req,res) => {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if(!user){
+        return res.status(400).json({
+            message: "User not found"
+        })
+    }
+
+    const isPasswordMatch = await user.password == crypto.createHash("md5").update(password).digest("hex");
+
+    if(!isPasswordMatch){
+        return res.status(400).json({
+            message: "invalid password"
+        })
+    }
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET_KEY,
+    )
+    res.cookie("jwt_token", token)
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user,
+        token
+    });
+})
+
+module.exports = authRouter;
